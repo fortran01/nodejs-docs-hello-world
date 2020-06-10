130ecbeaa75d4b99c09cd44f3e256f07bb62c861
Prem Rara <p@rara.dev>
2020-06-10 08:36:32 -0400

    Sample pattern to log to stdout with pinojs

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const logger = require('pino')()
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+	logger.info(request)
+	let body = '';
+	request.on('data', chunk => {
+		body += chunk.toString(); // convert Buffer to string
+	});
+	request.on('end', () => {
+		logger.info(body);
+	});
+	response.writeHead(200, {
+		"Content-Type": "text/plain"
+	});
+	response.end("Hello World!");
 });
 
 const port = process.env.PORT || 1337;
